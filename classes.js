ed14986996d51356bdded78713d2e9e050f975ce
Fralop <117324827+Fralop@users.noreply.github.com>
2025-01-07 23:22:10 -0500

    File duplication deleted

--- classes.js
+++
@@ -1,9 +0,0 @@
-const div = document.querySelector('div');
-div.className = 'warning';
-
-const activeDiv = document.querySelector('.active');
-
-activeDiv.classList.add('hidden');
-activeDiv.classList.remove('hidden');
-activeDiv.classList.toggle('hidden');
-activeDiv.classList.replace('active', 'warning');
